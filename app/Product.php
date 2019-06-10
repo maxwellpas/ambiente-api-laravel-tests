f778d56970a2351705d88e49a7e051477b6e57a1
Maxwell Pereira <maxwell.pereira@sympla.com.br>
2019-06-10 11:10:54 -0300

    Trabalhando com funcoes globais no Model

--- app/Product.php
+++ app/Product.php
@@ -3,11 +3,26 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Schema\Builder;
 
 class Product extends Model
 {
     protected $fillable= ['name', 'price', 'description'];
     
+
+    /**
+     * Aplicação de funções globais desatualizada
+    protected static function boot()
+    {
+        parent::boot();
+        
+        static::addGlobalScope('maioresQue150', function(Builder $builder){
+            $builder->where('id', '>', '150');
+
+        });
+    }
+     */
+
 
     /**
      * Tranbalhando com mutators, antes de ser registrado no banco de dados
